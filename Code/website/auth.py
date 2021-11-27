bbb8c45157acce566c62f5933c7292452696fba7
Stoisavljevic Aleksandar <h051589@stud.u-szeged.hu>
2021-11-27 01:43:24 +0100

    HTML & CSS & PY comunication with db login 60%

--- Code/website/auth.py
+++ Code/website/auth.py
@@ -1,14 +1,30 @@
 # standard routs for pages
-
-
-from flask import Blueprint, render_template, request, flash
+from flask import Blueprint, render_template, request, flash, redirect, url_for
+from website import dbapp
+from werkzeug.security import generate_password_hash, check_password_hash
+from flask_login import login_user, login_required, logout_user, current_user
 
 auth = Blueprint("auth", __name__)
 
 
-@auth.route("/login", methods=['POST'])
+@auth.route("/login", methods=['GET','POST'])
 def login():
-    return render_template("/login.html", text="testing")
+    if request.method=="POST":
+        email=request.form.get("email")
+        password=request.form.get("password")
+        user=dbapp.loginuser(email)
+        print(user)
+        if user!=False:
+            if check_password_hash(user["password"], password):
+                flash('Logged in successfully!', category='success')
+                login_user(user, remember=True)                     #-----------------------------treba class user da dobije
+                return redirect(url_for('views.home'))
+            else:
+                flash('Incorrect password, try again.', category='error')
+        else:
+            flash('Email does not exist.', category='error')
+
+    return render_template("login.html", user=current_user)
 
 
 @auth.route("/logout")
@@ -23,11 +39,26 @@ def sing_up():
         firstName = request.form.get("firstName")
         password1 = request.form.get("password1")
         password2 = request.form.get("password2")
-
-        if len(email) < 4:
-            flash("email must be greater  than 4 char", category='error')
+        user=dbapp.existuser(email)
+        if user:
+            flash('Email already exists.', category='error')
+        elif len(email) < 4:
+            flash('Email must be greater than 3 characters.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be greater than 1 character.', category='error')
+        elif password1 != password2:
+            flash('Passwords don\'t match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters.', category='error')
         else:
-            flash("acc created", category="success")
+            new_user=dict(email=email, firstName=firstName, password=generate_password_hash(
+                password1, method='sha256'))
+            # print(new_user)
+            if dbapp.addnewuser(new_user):
+                flash('Account created!', category='success')
+                return redirect(url_for('views.home'))
+            else:
+                flash("Account doesn't created! \n Try again later.", category='error')
 
 
     return render_template("/signup.html")
--- Code/website/dbapp.py
+++ Code/website/dbapp.py
@@ -1,26 +1,104 @@
 import mysql.connector
+
+
 def conect_to_db():
     try:  # ha nincs db vagy nem sikerul csatlakozni akor except
-            mydb = mysql.connector.connect(
-                host="localhost",
-                user="root",
-                database="famtree"
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            database="famtree"
 
-            )
-            mycursor = mydb.cursor()
-            return mycursor
+        )
+        mycursor = mydb.cursor()
+        email = "aleksandarfax@gmail.com"
+        sql = f"SELECT * FROM users WHERE users.email='{email}';"
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall()
+        print(myresult)
+        for x in myresult:
+            print(x)
+            print(type(x))
+            print(myresult)
+            # ----------------------------------------------------------------
+        return 0
 
     except:
         print("Nincs DB")
         return "NODB"
 
 
-def existuser(mydb,email):
-    mydb.execute("Show tables;")
+def existuser(email):
+    try:  # ha nincs db vagy nem sikerul csatlakozni akor except
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            database="famtree"
 
-    myresult = mydb.fetchall()
+        )
+        mycursor = mydb.cursor()
+        sql = f"SELECT * FROM users WHERE users.email='{email}';"
+        mycursor.execute(sql)
+        myresult = mycursor.fetchall()
+        if len(myresult) != 0:
+            # ----------------------------------------------------------------
+            return True
+        else:
+            return False
 
-    for x in myresult:
-        print(x[0])
-    return True
 
+    except:
+        print("Nincs DB")
+        return "NODB"
+
+
+def addnewuser(newuser):
+    try:  # ha nincs db vagy nem sikerul csatlakozni akor except
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            database="famtree"
+
+        )
+        mycursor = mydb.cursor()
+        if (type(newuser) == dict):
+            email = newuser["email"]
+            fname = newuser["firstName"]
+            password = newuser["password"]
+            sql = f"INSERT INTO `users` (`email`, `firstname`, `password`) VALUES ('{email}','{fname}','{password}');"
+            mycursor.execute(sql)
+            mydb.commit()
+            return True
+        else:
+            return False
+
+
+    except:
+        print("Nincs DB")
+        return False
+
+
+def loginuser(email):
+    try:  # ha nincs db vagy nem sikerul csatlakozni akor except
+        mydb = mysql.connector.connect(
+            host="localhost",
+            user="root",
+            database="famtree"
+
+        )
+
+        mycursor = mydb.cursor()
+        sql = f"SELECT * FROM `users` WHERE `users`.`email` = '{email}';"
+        mycursor.execute(sql)
+        myresults=mycursor.fetchall()
+        print(len(myresults))
+        if (len(myresults)) == 0:
+            print("dibije false")
+            return False
+        else:
+            user = {"email": myresults[0][0], "firstname": myresults[0][1],
+                    "password": myresults[0][2]}
+            return user
+
+    except:
+        print("Nincs DB")
+        return False
